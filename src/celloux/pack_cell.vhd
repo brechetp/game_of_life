dcbd19c39e4947047b5e1b2aacb0e9bb2baf0c17
Michael Mollard <mollard@jungfrau.eurecom.fr>
2015-05-22 11:01:29 +0200

    Conversion function

--- src/celloux/pack_cell.vhd
+++ src/celloux/pack_cell.vhd
@@ -1,12 +1,16 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
 package pack_cell is
 
-  type STATUS is (DEAD, ALIVE);
+  type STATUS is (NEWDEAD, DEAD, NEWALIVE, ALIVE);
   subtype N_COUNT is INTEGER range 0 to 8;
   subtype BIT_COUNT is BIT_VECTOR (1 downto 0);
   
   function "+"(S1, S2: STATUS) return N_COUNT;
   function "+"(N: N_COUNT; S: STATUS) return N_COUNT;
-  function invert(state: STATUS) return STATUS;
+  function color2state(cell, color: std_ulogic_vector) return STATUS;
+  function state2color(cell: STATUS; color: std_ulogic_vector) return std_ulogic_vector; 
 
 end package pack_cell;
 
@@ -16,10 +20,10 @@ package body pack_cell is
   function "+"(S1, S2: STATUS) return N_COUNT is
     variable SUM: N_COUNT := 0;
   begin
-    if S1 = ALIVE then
+    if (S1 = ALIVE) or (S1 = NEWALIVE) then
       SUM := SUM + 1;
     end if;
-    if S2 = ALIVE then
+    if S2 = ALIVE or S2 = NEWALIVE then
       SUM := SUM + 1;
     end if;
     return SUM;
@@ -28,23 +32,27 @@ package body pack_cell is
   function "+"(N: N_COUNT; S: STATUS) return N_COUNT is
     variable SUM: N_COUNT := N;
   begin
-    if S = ALIVE then
+    if S = ALIVE or S = NEWALIVE then
       SUM := SUM + 1;
     end if;
     return SUM;
   end "+";
-  
--- invert signal function
-  function invert(state: STATUS) return STATUS is
-    variable new_state: STATUS;
+
+  function color2state(cell, color: std_ulogic_vector) return STATUS is
+    variable state: STATUS := DEAD;
   begin
-    if state = ALIVE then
-      new_state := DEAD;
-    else
-      new_state := ALIVE;
-    end if;
-    return new_state;
-  end invert;
+    for i in 0 to 3 loop
+      if cell = color(8*i+7 downto 8*i) then
+        state := STATUS'VAL(i);
+      end if;
+    end loop;
+    return state;
+  end color2state;
+
+  function state2color(cell: STATUS; color: std_ulogic_vector) return std_ulogic_vector is
+  begin
+    return color(8*STATUS'POS(cell)+7 downto 8*STATUS'POS(cell));
+  end state2color;
 
 end package body pack_cell;
 
