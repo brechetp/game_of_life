eb328f9d1d1446b37270c2836e244b7a38de4f99
mollard <mollard@eurecom.fr>
2015-06-01 01:07:18 +0200

    master implemented waiting for test

--- hdl/axi_register/axi_register_master.vhd
+++ hdl/axi_register/axi_register_master.vhd
@@ -19,118 +19,126 @@ use global_lib.utils.all;
 library axi_lib;
 use axi_lib.axi_pkg.all;
 
--- See the README file for a detailed description of the AXI register
+library celloux_lib;
+use celloux_lib.pack_cell.all
+
+-- See the README file for a detailed description of the AXI register master
 
 entity axi_register_master is
-  generic(na1: natural := 30;   -- Number of significant bits in S_AXI addresses (12 bits => 4kB address space)
-          nr1: natural := 2);   -- Number of 32-bits registers in S_AXI address space; addresses are 0 to 4*(nr1-1)
   port(
-    aclk:       in std_ulogic;
-    aresetn:    in std_ulogic;
+    aclk:       in  std_ulogic;
+    aresetn:    in  std_ulogic;
     -- AXI lite slave port
-    s_axi_m2s:  in  axilite_gp_m2s;
-    s_axi_s2m:  out axilite_gp_s2m;
+    m_axi_m2s:  out axi_gp_m2s;
+    m_axi_s2m:  in  axi_gp_s2m;
     -- Control signals 
-    address:    in  std_ulogic_vector(31 downto 0); -- Address from which to start reading
-    address_rst:in  std_ulogic;                     -- Address reset to empty out buffers and read address is valid
-    start:      in  std_ulogic;                     -- Start signal to start everything
+    waddress:   in  std_ulogic_vector(31 downto 0); --	Address from which to start writing
+    raddress:	in  std_ulogic_vector(31 downto 0); --	Address from which to start reading
+    wsize:	in  std_ulogic_vector(7 downto 0);  --	size of writting burst
+    rsize:	in  std_ulogic_vector(7 downto 0);  --	size of reading burst
     -- GPIO
-    gpi:        in  std_ulogic_vector(63 downto 0); -- 8 cell to be written in memory
-    gpi_valid:  in  std_ulogic;                     -- Input data is valid
-    gpo:        out std_ulogic                      -- Write succesfull, ready for another
+    wc_vector:	    in  cell_vector;		    --	cell array to be written in memory
+    write_rq:	    in  std_ulogic;                 --	Input data is valid - request new write
+    read_rq :	    in  std_ulogic:		    --  request new read
+    done_writing:   out std_ulogic;                 --	Write succesfull, ready for another
+    done_reading:   out std_ulogic;		    --	Read finished
+    rc_vector:	    out cell_vector		    --	cell array to be read from
+    w_offset:	    in	integer range 0 to 79;	    --	offset from which to write in rc_vector
   );
 end entity axi_register;
 
 architecture rtl of axi_register_master is
-
-  constant l2nr1: natural := log2_up(nr1); -- Log2 of nr1 (rounded towards infinity)
-
-  constant gpir_idx: natural range 0 to nr1 - 1 := 0;
-  constant gpor_idx: natural range 0 to nr1 - 1 := 1;
-
-  constant roreg: std_ulogic_vector(0 to nr1 - 1) := "10";
-
-  subtype reg_type is std_ulogic_vector(31 downto 0);
-  type reg_array is array(0 to nr1 - 1) of reg_type;
-  signal regs: reg_array;
-
+    shared variable done_writing_tmp: std_ulogic;
+    shared variable done_reading_tmp: std_ulogic;
 begin
-    
-  regs_pr: process(aclk)
-    -- idle: waiting for AXI master requests: when receiving write address and data valid (higher priority than read), perform the write, assert write address
-    --       ready, write data ready and bvalid, go to w1, else, when receiving address read valid, perform the read, assert read address ready, read data valid
-    --       and go to r1
-    -- w1:   deassert write address ready and write data ready, wait for write response ready: when receiving it, deassert write response valid, go to idle
-    -- r1:   deassert read address ready, wait for read response ready: when receiving it, deassert read data valid, go to idle
-    type state_type is (idle, w1, r1);
-    variable state: state_type;
-    variable wok, rok: boolean;                           -- Write (read) address mapped
-    variable widx, ridx: natural range 0 to 2**l2nr1 - 1; -- Write (read) register index
+  -- This process will emit the request over the axi bus
+  wnr_request_pr: process(aclk)
+    -- idle:	waiting for gpi_valid to write or gpi_nr to read (higher priority to write)
+    -- if gpi_valid is set to 1 then initialize variable then we set do_write_rq, if gpi_nr is set to 1 then we set do_reqd_rq
+    -- do_write_rq:	if awready is 1 then assert awvalid (to 1), awaddr (to address), awlen (to the burst size = 10),awsize (011b = 8 bytes (64-bit wide burst)), awburst (type of burst, only 01b = INCR ) for one clock yhen go back to idle
+    -- do_read_rq:	if arready is 1 then assert arvalid (to 1), araddr (to address), arlen (to the burst size = 10),arsize (011b = 8 bytes (64-bit wide burst)), arburst (type of burst, only 01b = INCR ) for one clock yhen go back to idle
+    variable do_write_rq:   std_ulogic;
+    variable do_read_rq:    std_ulogic;
   begin
     if rising_edge(aclk) then
       if aresetn = '0' then
-        regs <= (others => (others => '0'));
-        s_axi_s2m <= (rdata => (others => '0'), rresp => axi_resp_okay, bresp => axi_resp_okay, others => '0');
-        state := idle;
+	do_write_rq :=	'0';
+	do_read_rq  :=	'0';
       else
-        regs(gpir_idx) <= X"000000" & gpi; -- General purpose inputs
-
-        -- Addresses ranges
-        widx := to_integer(unsigned(s_axi_m2s.awaddr(l2nr1 + 1 downto 2)));
-        ridx := to_integer(unsigned(s_axi_m2s.araddr(l2nr1 + 1 downto 2)));
-
-        -- S_AXI write and read
-        case state is
-          when idle =>
-            if s_axi_m2s.awvalid = '1' and s_axi_m2s.wvalid = '1' then -- Write address and data
-              if or_reduce(s_axi_m2s.awaddr(na1 - 1 downto l2nr1 + 2)) /= '0' or widx >= nr1 then -- If unmapped address
-                s_axi_s2m.bresp <= axi_resp_decerr;
-              elsif roreg(widx) = '1' then -- If read-only register
-                s_axi_s2m.bresp <= axi_resp_slverr;
-              else
-                for i in 0 to 3 loop
-                  if s_axi_m2s.wstrb(i) = '1' then
-                    regs(widx)(8 * i + 7 downto 8 * i) <= s_axi_m2s.wdata(8 * i + 7 downto 8 * i);
-                  end if;
-                end loop;
-              end if;
-              s_axi_s2m.awready <= '1';
-              s_axi_s2m.wready <= '1';
-              s_axi_s2m.bvalid <= '1';
-              state := w1;
-            elsif s_axi_m2s.arvalid = '1' then
-              if or_reduce(s_axi_m2s.araddr(na1 - 1 downto l2nr1 + 2)) /= '0' or ridx >= nr1 then -- If unmapped address
-                s_axi_s2m.rdata <= (others => '0');
-                s_axi_s2m.rresp <= axi_resp_decerr;
-              else
-                s_axi_s2m.rdata <= regs(ridx);
-              end if;
-              s_axi_s2m.arready <= '1';
-              s_axi_s2m.rvalid <= '1';
-              state := r1;
-            end if;
-          when w1 =>
-            s_axi_s2m.awready <= '0';
-            s_axi_s2m.wready <= '0';
-            if s_axi_m2s.bready = '1' then
-              s_axi_s2m.bvalid <= '0';
-              s_axi_s2m.bresp <= axi_resp_okay;
-              state := idle;
-            end if;
-          when r1 =>
-            s_axi_s2m.arready <= '0';
-            if s_axi_m2s.rready = '1' then
-              s_axi_s2m.rvalid <= '0';
-              s_axi_s2m.rresp <= axi_resp_okay;
-              state := idle;
-            end if;
-        end case;
+	m_axi_m2s.awvalid <= '0';
+	m_axi_m2s.arvalid <= '0';
+	if write_rq = '1' then
+	  do_write_rq	    := '1';
+	  done_writing_tmp  := '0';
+	elsif read_rq = '1' then
+	  do_read_rq	    := '1';
+	  done_reading_tmp  := '0';
+	end if;
+	if do_write_rq = '1' then
+	  if m_axi_s2m.awready <= '1' then
+	    m_axi_m2s.awvalid	<= '1';
+	    m_axi_m2s.awaddr	<= address;
+	    m_axi_m2s.awlen	<= 10;
+	    m_axi_m2s.awsize	<= wsize;
+	    m_axi_m2s.awburst	<= axi_burst_incr;
+	    do_write_rq		:= '0';
+	  end if;
+	end if;
+	if do_read_rq = '1' then
+	  if m_axi_s2m.arready <= '1' then
+	    m_axi_m2s.arvalid	<= '1';
+	    m_axi_m2s.araddr	<= address;
+	    m_axi_m2s.arlen	<= 10;
+	    m_axi_m2s.arsize	<= rsize;
+	    m_axi_m2s.arburst	<= axi_burst_incr;
+	    do_read_rq		:= '0';
+	  end if;
+	end if;
       end if;
     end if;
-  end process regs_pr;
+  end process wnr_request_pr;
 
-  with regs(gpir_idx)(7 downto 0) select
-    gpo <= regs(gpor_idx)(7 downto 0) when X"00",
-           X"55" when others;
+  -- This process drive done_reading and done_writing
+  rw_drive_proc: process (aclk)
+  begin
+    if rising_edge(aclk) then
+      done_reading <= done_reading_tmp;
+      done_writing <= done_writing_tmp;
+    end if;
+  end process rw_drive_proc;
 
+  --This process will emit and read the data as needed. 
+  wr_proc: process(aclk)
+    -- idle:	wait for either rvalid or wvalid, set wdata to wc_vector(write_cpt)
+    -- rvalid:	store convert2cell(rdata(i)) in rc_vector(read_cpt+i) 8 time (as there are 8 cell in 64bit) and incr read_cpt
+    --		if rlast is set, reset read_cpt, drive done_reading_tmp to 1
+    -- wvalid:	incr write_cpt by 8
+    --		if write_cpt = wsize then set wlast and done_writing_tmp to 1
+    variable write_cpt,read_cpt: natural;
+  begin
+    if rising_edge(aclk)
+      for i in 0 to 7 loop
+        m_axi_m2s.wdata(8*i+7 downto 8*i) <= convert2color(wc_vector(8*write_cpt + i));
+      end loop;
+      if m_axi_s2m.rvalid = '1' then
+        for i in 0 to 7 loop
+         rc_vector(8*read_cpt+i) <= convert2cell(m_axi_s2m.rdata(8*i+7 downto 8*i));
+        end loop;
+        read_cpt := read_cpt + 1;
+	if m_axi_s2m.rlast = '1' then
+	  done_reading_tmp := '1';
+	  read_cpt := 0;
+	end if;
+      end if;
+      if m_axi_s2m.wvalid = '1' then
+        if write_cpt = wsize then
+          m_axi_m2s.wlast = '1';
+	  done_writing_tmp := '1';
+	  write_cpt := 0;
+	else
+	  write_cpt := write_cpt + 1;
+	end if;
+      end if;
+    end if;
+  end process wr_proc;
 end architecture rtl;
