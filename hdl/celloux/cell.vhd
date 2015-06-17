ca34fda8dfed2b2af41c437c9589fec04bfc8089
Pierre Bréchet <brechetp@eurecom.fr>
2015-06-17 10:58:52 +0200

    latch inference resolution on cell_ctrl

--- hdl/celloux/cell.vhd
+++ hdl/celloux/cell.vhd
@@ -12,7 +12,7 @@ entity cell is
     run: in std_ulogic; -- if unset the state_out is a copy of mem_state
     N, NE, E, SE, S, SW, W, NW: in CELL_STATE; -- the cell neighbors
     self: in CELL_STATE; -- the gen n state
-    state_out: out CELL_STATE
+    state_out: out CELL_STATE -- the gen n+1 state
   );
 end entity cell;
   
@@ -20,38 +20,43 @@ end entity cell;
 
 architecture syn of cell is
 
+  signal next_state: CELL_STATE;
+
 begin
 
+
+  state_out <= next_state;
 
   process(clk)
     variable neighbour_count: BIT_COUNT; -- gives the count of neighbours
   begin
     if clk = '1' then -- on rising edge, we assume neighbors are ready
       if rstn = '0' then -- the reset is set
+        next_state <= DEAD;
         state_out <= DEAD;
       else
         if run = '1' then -- we output something only if the run flag is set
           neighbour_count := three_count(N, NE, E, SE, S, SW, W, NW); -- we redefined the add operation
           case self is -- we check the old state
             when DEAD =>
-              state_out <= DEAD; -- default case
+              next_state <= DEAD; -- default case
               if neighbour_count(0) = '1' and neighbour_count(1) = '1' then -- reproduction
-                state_out <= NEWALIVE;
+                next_state <= NEWALIVE;
               end if;
             when ALIVE =>
-              state_out <= ALIVE; -- default case
+              next_state <= ALIVE; -- default case
               if neighbour_count(1) = '0' then -- count /= 2,3 -> death
-                state_out <= NEWDEAD;
+                next_state <= NEWDEAD;
               end if;
             when NEWDEAD =>
-              state_out <= DEAD; -- default case
+              next_state <= DEAD; -- default case
               if neighbour_count(0) = '1' and neighbour_count(1) = '1' then -- count =3 -> reproduction
-                state_out <= NEWALIVE;
+                next_state <= NEWALIVE;
               end if;
             when NEWALIVE =>
-              state_out <= ALIVE;
+              next_state <= ALIVE;
               if neighbour_count(1) = '0' then --count /= 2,3  death
-                state_out <= NEWDEAD;
+                next_state <= NEWDEAD;
               end if;
             end case ; -- end of the case switch
           end if;
